52041a102821e2cf4f2fa8007aa856e190e8dc10
Aluno <senai@senai.com>
2026-07-28 09:07:17 -0300

    Ajuste Final da Tabela

--- tabela-estoque.sql
+++ tabela-estoque.sql
@@ -5,17 +5,16 @@ CREATE TABLE estoque (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
     quantidade INT,
-    estoque_minimo INT,
-    descricao VARCHAR(255),
     preco DECIMAL(10,2),
     categoria VARCHAR(50),
-    status_item VARCHAR(50),
+    estoque_minimo INT,
+    descricao_adicional VARCHAR(255),
     foto VARCHAR(255)
 );
 
 /*comando para inserir itens na tabela manualmente pelo mysql workbench*/
-INSERT INTO estoque (id, nome, quantidade)
-VALUES (1, 'parafusos', '100');
+INSERT INTO estoque (id, nome, quantidade, preco, categoria, estoque_minimo, descricao_adicional)
+VALUES (1, 'parafusos', '2000', '0.15', 'Geral', '100', 'tem de vários tamanhos, é de metal');
 
 /*comando para ver a tabela no mysql workbench*/
 SELECT * FROM estoque;
