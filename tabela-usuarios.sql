3b888fede00162f7fb1394cb592bf7664b567acb
Aluno <senai@senai.com>
2026-07-29 08:31:05 -0300

    Ajustes Finais

--- tabela-usuarios.sql
+++ tabela-usuarios.sql
@@ -8,4 +8,8 @@ CREATE TABLE usuarios (
     papel VARCHAR(30) NOT NULL
 );
 
+/*comando para ver a tabela no mysql workbench*/
 SELECT * FROM usuarios;
+
+/*serve para apagar todos os dados inseridos na tabela (só execute esse se algo der errado e quiser resetar os dados)*/
+TRUNCATE TABLE usuarios;
